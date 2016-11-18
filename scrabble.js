d2707dcf26e72d2daf092962a2484ccb60fa3499
Nina Mutty <ninamutty@gmail.com>
2016-11-18 09:32:29 -0800

    tilebag draw method complete

--- scrabble.js
+++ scrabble.js
@@ -57,58 +57,11 @@ Scrabble.prototype = {
   }
 };
 
-// YOUR CODE HERE
-// Scrabble.prototype.helloWorld = function() {
-//   return 'hello world!';
-// };
 
 module.exports = Scrabble;
 
-/////////////////////////////
-//////// Player /////////////
-/////////////////////////////
 
-// var Player = function(name) {
-//   this.name = name;
-//   this.plays = [];
-// };
-//   // Put all of this in prototype
-// Player.prototype = {
-//   play: function(add) {
-//     return this.plays.push(add);
-//   },
-//
-//   totalScore: function() {
-//     total = 0;
-//     scrabble = new Scrabble();
-//     for(var i=0; i < this.plays.length; i++) {
-//       total += scrabble.score(this.plays[i]);
-//     };
-//     return total;
-//   },
-//
-//   hasOne: function() {
-//     if (this.totalScore(this.plays) >= 100) {
-//       return true;
-//     } else {
-//       return false;
-//     };
-//   },
-//
-//   highestScoringWord: function() {
-//     scrabble = new Scrabble();
-//     return scrabble.highestScoreFrom(this.plays);
-//   },
-//
-//   highestWordScore: function() {
-//     scrabble = new Scrabble();
-//     return scrabble.score(scrabble.highestScoreFrom(this.plays));
-//   }
-// };
-//
-// // Player.prototype.plays = function() {
-// //   return plays
-// // }
+
 console.log("####################################")
 console.log("########## SCRABBLE TESTS ##########");
 console.log("####################################")
@@ -122,23 +75,3 @@ console.log(s.highestScoreFrom(["aaaaad", "aaaaaaa"])); // 'aaaaaaa'
 console.log(s.score(""));
 console.log("Cat: " + s.score("cat"));
 console.log(s.highestScoreFrom(["", ""]));
-//
-//
-// console.log("####################################")
-// console.log("########## PLAYER TESTS ##########");
-// console.log("####################################")
-// nina = new Player("Nina");
-// console.log(nina.name);
-// console.log(nina.plays);
-//
-// nina.play("cat");
-// nina.play("dog");
-// nina.play("fish");
-// console.log(nina.plays);
-// console.log("Total Score: " + nina.totalScore());
-// console.log("Has One: " + nina.hasOne());
-// nina.play("ZZZQZZZ");
-// console.log("Total Score: " + nina.totalScore());
-// console.log("Has One: " +  nina.hasOne());
-// console.log("Highest Scoring Word: " + nina.highestScoringWord());
-// console.log("Highest Word Score: " + nina.highestWordScore());
